705dc53cc5b2ee94c5b5cc3b005b8203a2c16ff9
Rogerio Junior <rogerio.junior1016@gmail.com>
2021-05-21 17:44:26 -0300

    Quicksort 1 	# Please enter the commit message for your changes. Lines starting

---
+++ quicksort.py
@@ -0,0 +1,26 @@
+def quicksort(lista, primeiro, ultimo):
+    if primeiro < ultimo:
+        pivo = lista[primeiro]
+        leftmark = primeiro+1
+        rightmark = ultimo
+        while True:
+            while leftmark <= rightmark and lista[leftmark] <= pivo:
+                leftmark+= 1
+            while rightmark >= leftmark and lista[rightmark] >= pivo:
+                rightmark-= 1
+            if rightmark < leftmark:
+                break
+            else:
+                temp = lista[rightmark]
+                lista[rightmark] = lista[leftmark]
+                lista[leftmark] = temp
+        temp = lista[rightmark]
+        lista[rightmark] = pivo
+        lista[primeiro] = temp
+        quicksort(lista, primeiro, rightmark-1)
+        quicksort(lista, rightmark+1, ultimo)
+def quicksorthelper(lista):
+    quicksort(lista, 0, len(lista)-1)
+lista = [10,5, 11, 6, 5, 4, 3, 2, 1]
+quicksorthelper(lista)
+print(lista)
